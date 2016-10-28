9473ce3118c610c3b21c43d57e0c1561d61fea78
Syed Ali <sali3@cox.net>
2016-10-28 05:51:07 -0400

    Working on function to add a new line item; created variables to work with

--- js/ui.js
+++ js/ui.js
@@ -4,14 +4,31 @@
 
   console.log("I am js ui file");
 
+// Function creation to add new todo list item
+  function newElement() {
+    // For creation of new li
+    var li = document.createElement("li");
+    //
+    var inputValue = document.getElementsByClassName(".to-do").value;
+    var newTodo = document.createTextNode(inputValue)
+    //Adding text to new todo line item.
+    li.appendChild(newTodo);
+    // Adding new todo list item to UL
+    document.getElementsByClassName(".")
+
+
+    document.getElementsByClassName(".items").appendChild(li);
+    document.getElementsByClassName(".to-do").value = "";
+
+  }
+
+
+/////////////////////////////////////
   document
     .querySelector(".new-todo")
     .addEventListener("click", function addListItem (eventObj) {
     eventObj.target.style.color = "green";
     })
-
-
-
 
 
 
