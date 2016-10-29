411164c435ec4bd1a617f9e5ad7f09f9932b31db
Syed Ali <sali3@cox.net>
2016-10-29 13:35:07 -0400

    Refactored my code and added comments

--- js/ui.js
+++ js/ui.js
@@ -5,47 +5,66 @@
 
   document
     .querySelector("form")
-    .addEventListener("submit", function addListItem(eventObj) {
+    .addEventListener("submit", function(event){
 
-      eventObj.preventDefault();
+      // To prevent default and JS to implement
+      event.preventDefault();
 
-      var li = document.createElement("li");
-      var inputValue = document.querySelector(".new-todo").value;
+      // Creatiion of li element to attach to ul
+      var newList = document.createElement("li");
 
-      var addArticle = document.createElement("article");
+      // To capture form input
+      var newTodo = document.querySelector(".new-todo").value;
 
-
-      var addButton = document.createElement("button");
-      addButton.classList.add("check");
-      addArticle.appendChild(addButton);
-
-      var addP = document.createElement("p");
-      addP.innerText = inputValue;
-      addArticle.appendChild(addP);
-
-      var addXbutton = document.createElement("button");
-      addXbutton.classList.add("delete");
-      addXbutton.innerText = "X";
-      addArticle.appendChild(addXbutton);
-
-      li.appendChild(addArticle);
-
-      document.querySelector(".items").appendChild(li);
+      // Creatiion of artilce element to attach to  li
+      var newArticle = document.createElement("article");
 
 
-    })
+      //Creation of check button element to attach to article
+      var newCheckButton = document.createElement("button");
+      // Assignment of class to check button for style to take hold
+      newCheckButton.classList.add("check");
+      // Appending to article
+      newArticle.appendChild(newCheckButton);
 
 
-  document.querySelector("article")
-  .addEventListener("click", function complete(event) {
-  event.target.classList.add("completed");
-  console.log("I am here");
+      // Creatiion of paragraph element to attach to article
+      var newParagraph = document.createElement("p");
+      newParagraph.innerText = newTodo;
+      // Appending to article
+      newArticle.appendChild(newParagraph);
+
+
+      var newDeleteButton = document.createElement("button");
+      // Assignment of class to delete button for style to take hold
+      newDeleteButton.classList.add("delete");
+      // Appending to article
+      newArticle.appendChild(newDeleteButton);
+      newDeleteButton.innerText = "X";
+
+
+      // Appending article to li
+      newList.appendChild(newArticle);
+
+      //Appending li to ul
+      document.querySelector(".items").appendChild(newList);
 
 
 
 
+    });
 
-  })
+
+//   document.querySelector(".check")
+//   .addEventListener("click", function complete(event) {
+//   event.target.classList.add("completed");
+//   console.log("I am here");
+//
+//
+//
+//
+//
+// });
 
 
 
